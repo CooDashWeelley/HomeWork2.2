eb7af4ec3ba43c0057f93b008726c9c987be160f
CooDashWeelley <iwan.kudashow@gmail.com>
2025-07-24 23:12:54 +0300

    edit 3

--- src/PrintService.java
+++ src/PrintService.java
@@ -3,9 +3,9 @@ public class PrintService {
         if (totalA > totalB) {
             System.out.println(a.getName() + " better in " + faculty + " than " + b.getName() + " with " + totalA + " points");
         } else if (totalA < totalB) {
-            System.out.println(b.getName() + " better in Gryffindor than " + a.getName() + " with " + totalB + " points");
+            System.out.println(b.getName() + " better in " + faculty + " than " + a.getName() + " with " + totalB + " points");
         } else {
-            System.out.println("students in Gryffindor equals with " + totalA + " and " + totalB + " points");
+            System.out.println("students in " + faculty + " equals with " + totalA + " and " + totalB + " points");
         }
     }
 }
